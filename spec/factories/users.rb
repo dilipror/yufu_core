fc9bd5f9be07dbcb3241de3094805afeb59d92f2
leo <mvleo.github@gmail.com>
2015-09-25 09:07:02 +0600

    fix queue specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com"}
-    sequence(:phone) {|n| "911#{n}"}
+    sequence(:phone) {|n| "0911#{n}"}
     confirmed_at Date.yesterday
     password 'password'
   end
