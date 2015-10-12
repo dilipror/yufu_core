003e87c38e4284ebff7c9c12029027daca6d9ded
mitchenkod <mitchenko1897@mail.ru>
2015-10-12 18:43:26 +0600

    #y-690 work 3h

--- app/jobs/close_unpaid_job.rb
+++ app/jobs/close_unpaid_job.rb
@@ -2,8 +2,8 @@ class CloseUnpaidJob < ActiveJob::Base
   queue_as :default
 
   def perform(order_id)
-    if state == 'paying' || state == 'new'
-      order = Order::Base.find order_id
+    order = Order::Base.find order_id
+    if order.state == 'paying' || order.state == 'new'
       klass = "::#{order._type}::RejectService"
       service = klass.constantize.new(order)
       service.reject_order :yufu
