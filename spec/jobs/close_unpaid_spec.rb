b41e70c4de2caac5269a3ca7b9121205f09164db
mitchenkod <mitchenko1897@mail.ru>
2015-10-13 12:57:08 +0600

    #y-690 fixed

---
+++ spec/jobs/close_unpaid_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+RSpec.describe OrderCleanerWorker, :type => :worker do
+  describe '#perform' do
+
+    subject{CloseUnpaidJob.new.perform(order.id.to_s)}
+
+    context 'order verbal' do
+
+      context 'order is paid' do
+
+        let(:translator){create :profile_translator}
+        let(:order){create :order_verbal, invoices: [(create :invoice)], assignee: translator, state: 'wait_offer'}
+
+        it{expect{subject}.not_to change{order.reload.state}}
+      end
+
+      context 'order is not paid' do
+
+        let(:translator){create :profile_translator}
+        let(:order){create :order_verbal, invoices: [(create :invoice)], assignee: translator, state: 'paying'}
+
+        it{expect{subject}.to change{order.reload.state}.to 'rejected'}
+      end
+
+    end
+
+    context 'order written' do
+
+      context 'order is paid' do
+
+        let(:order){create :order_written, state: 'wait_offer'}
+
+        it{expect{subject}.not_to change{order.reload.state}}
+      end
+
+      context 'order is not paid' do
+
+        let(:order){create :order_written, state: 'paying'}
+
+        it{expect{subject}.to change{order.reload.state}.to 'rejected'}
+
+      end
+
+    end
+  end
+end
