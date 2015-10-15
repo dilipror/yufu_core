2fd83312c8f0d26bd38966dd866ec70071239a98
leo <mvleo.github@gmail.com>
2015-10-15 12:37:35 +0600

    relation ticket-order in crm

--- app/models/support/theme.rb
+++ app/models/support/theme.rb
@@ -11,9 +11,10 @@ module Support
     increments :number
 
     enumerize :type, in: [:custom, :local_expert, :no_translator_found, :no_offers_confirmed]
-    enumerize :theme_type, in: [:custom, :local_expert]
+    enumerize :theme_type, in: [:custom, :local_expert, :order_written]
     scope :custom,           -> {where theme_type: :custom}
     scope :for_local_expert, -> {where theme_type: :local_expert}
+    scope :for_order_written, -> {where theme_type: :order_written}
 
 
     validates :name, presence: true, uniqueness: true
--- app/services/order/written/events_service.rb
+++ app/services/order/written/events_service.rb
@@ -7,75 +7,37 @@ module Order
 
       def after_paid_order
         OrderWrittenQueueFactoryWorker.new.perform @order.id, I18n.locale
-
-        # if @order.translation_language.is_chinese
-        #   if (Profile::Translator.approved.chinese.support_written_order(@order)).count > 0
-        #     OrderWrittenQueueFactoryWorker.new.perform @order.id, I18n.locale
-        #     # OrderWrittenWorkflowWorker.perform_in 1.minutes, @order.id, 'confirmation_order_in_30'
-        #   else
-        #     if (Profile::Translator.approved.support_written_order(@order).where(:'profile_steps_service.hsk_level'.gt => 4)).count > 0
-        #       OrderWrittenQueueFactoryWorker.new.perform @order.id, I18n.locale
-        #       # MAIL to TR - NEW ORDER AVAIL
-        #       # wait translation
-        #     else
-        #       @order.notify_about_cancellation_by_yufu
-        #     end
-        #   end
-        # else
-        #   if (Profile::Translator.approved.support_written_order(@order).where(:'profile_steps_service.hsk_level'.gt => 4)).count > 0
-        #     OrderWrittenQueueFactoryWorker.new.perform @order.id, I18n.locale
-        #     # @order.notify_about_new_order_available
-        #     # MAIL to TR - NEW ORDER AVAIL
-        #     # wait translation
-        #   else
-        #     @order.notify_about_cancellation_by_yufu
-        #   end
-        # end
-      end
-
-      def after_proof_reading
-        # 70 / 40 to proof reader
-        # qc
       end
 
       def after_translate_order
         if @order.translation_language.is_chinese
           if @order.assignee.chinese?
-            # 70% to tr
             if @order.need_proof_reading?
-              Support::Ticket.create text: 'written order', theme: Support::Theme.first,
-                  subject: 'some subject'
+              Support::Ticket.create text: I18n.t('frontend.order.writt.default_text_for_ticket'),
+                                     theme: Support::Theme.for_order_written.first,
+                                     subject: I18n.t('frontend.order.writt.default_subject_for_ticket'),
+                                     order: @order
               @order.state_event = 'correct'
-
-              # wait proofreading by BO
             else
               @order.state_event = 'control'
-
-              # qc
             end
           else#обязательно пруф ридинг тк переводил не китаец
-            Support::Ticket.create text: 'written order', theme: Support::Theme.first,
-                                   subject: 'some subject'
+            Support::Ticket.create text: I18n.t('frontend.order.writt.default_text_for_ticket'),
+                                   theme: Support::Theme.for_order_written.first,
+                                   subject: I18n.t('frontend.order.writt.default_subject_for_ticket'),
+                                   order: @order
             @order.state_event = 'correct'
-
-            # 60% to tr
-            # wait proofreading by BO
           end
         else# from ch to any
-          # 70% to tr
           if @order.need_proof_reading?
             if @order.assignee.can_proof_read? @order.translation_language
               @order.state_event = 'correct'
               @order.proof_reader = @order.assignee
-              #transition to state proof_read
             else
               @order.state_event = 'waiting_correcting'
-              # ГОНКА ЗА ПРУФ РИДИНГ
-              # MAIL to TR - NEW ORDER AVAIL
             end
           else
             @order.state_event = 'control'
-            # qc
           end
         end
 
@@ -88,8 +50,6 @@ module Order
         else
           if (Profile::Translator.approved.support_written_order(@order).where(:'profile_steps_service.hsk_level'.gt => 4)).count > 0
             OrderWrittenQueueFactoryWorker.new.perform @order.id, I18n.locale
-            # MAIL to TR - NEW ORDER AVAIL
-            # wait translation
           else
             @order.notify_about_cancellation_by_yufu
           end
