0bb8a7e6a131e2d4d592b94085a1f13c9340ce81
maxk <maxim21214@gmail.com>
2015-11-09 16:42:35 +0600

    #y-782 fixed

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,9 +4,38 @@ class NotificationMailer < ActionMailer::Base
   include ActionView::Helpers::UrlHelper
   include Devise::Controllers::UrlHelpers
 
+  def signup_reminder(user)
+    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
+                                      confirmation_url:  confirmation_url(user, confirmation_token: user.confirmation_token) )
+  end
 
   def cancel_not_paid_3(user)
     mail to: user.email, body: I18n.t('.body', scope: scope, root: root_url, client: client(user))
+  end
+
+  #old: for_client
+  def order_details_4(user, offer)
+    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user), client_id: offer.order.number,
+                                      order_details: order_details(offer.order),
+                                      interpreter_link: "#{Rails.application.config.host}/#{asset_host}/get_pdf_translator/#{offer.translator.id.to_s}.pdf",
+                                      dashboard_link: dashboard_link)
+  end
+
+  #old: check_dates
+  def check_dates_5(user, order)
+    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user), client_id: order.number,
+                                      interpreter_name: "#{ order.primary_offer.try(:translator).try(:user).try(:last_name)} #{order.primary_offer.try(:translator).try(:user).try(:last_name)}",
+                                      phone_number: "#{ order.primary_offer.try(:translator).try(:user).try(:phone)}",
+                                      order_details: order_details(order),
+                                      dashboard_link: dashboard_link)
+  end
+
+  def re_confirmed_client_6(user, offer)
+    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user), client_id: offer.order.number,
+                                      order_details: order_details(offer.order),
+                                      interpreter_link: "#{Rails.application.config.host}/#{asset_host}/get_pdf_translator/#{offer.translator.id.to_s}.pdf",
+                                      dashboard_link: dashboard_link)
+
   end
 
   def order_confirmation_7(user)
@@ -21,73 +50,8 @@ class NotificationMailer < ActionMailer::Base
     mail to: user.email, body: I18n.t('.body', scope: scope, dashboard_link: dashboard_link, client: client(user))
   end
 
-  def cancel_by_user_13(user)
-    mail to: user.email, body: I18n.t('.body', scope: scope, dashboard_link: dashboard_link, client: client(user))
-  end
-
-  def cancel_by_user_due_conf_delay_14(user)
-    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user))
-  end
-
-  def new_order_for_translator_16(user)
-    mail to: user.email, body: I18n.t('.body', scope: scope, dashboard_link: dashboard_link, client: client(user))
-  end
-
-  def signup_reminder(user)
-    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
-                                      confirmation_url:  confirmation_url(user, confirmation_token: user.confirmation_token) )
-  end
-
-  def translator_approving_15(user)
-    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
-                                      dashboard_link: dashboard_link)
-  end
-
-  # old: become_main_int
-  def become_main_int_17(user)
-    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
-                                      dashboard_link: dashboard_link)
-  end
-
-  #old: become_back_up_int
-  def become_back_up_int_18(user)
-    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
-                                      dashboard_link: dashboard_link)
-
-  end
-
-  #old: for_client
-  def order_details_4(user, offer)
-    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user), client_id: offer.order.number,
-                                      order_details: order_details(offer.order),
-                                      interpreter_link: "#{Rails.application.config.host}/#{asset_host}/get_pdf_translator/#{offer.translator.id.to_s}.pdf",
-                                      dashboard_link: dashboard_link)
-
-  end
-
   #old: we_are_looking
   def we_are_looking_10(user)
-    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
-                                      dashboard_link: dashboard_link)
-  end
-
-  #old: check_dates
-  def check_dates_5(user, order)
-    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user), client_id: order.number,
-                                      interpreter_name: "#{ order.primary_offer.try(:translator).try(:user).try(:last_name)} #{order.primary_offer.try(:translator).try(:user).try(:last_name)}",
-                                      phone_number: "#{ order.primary_offer.try(:translator).try(:user).try(:phone)}",
-                                      order_details: order_details(order),
-                                      dashboard_link: dashboard_link)
-
-  end
-
-  #old: re_confirm_main
-  def re_confirm_main_19(user)
-    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
-                                      dashboard_link: dashboard_link)
-  end
-
-  def re_confirm_back_up_20(user)
     mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
                                       dashboard_link: dashboard_link)
   end
@@ -100,19 +64,57 @@ class NotificationMailer < ActionMailer::Base
     mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user))
   end
 
+  def cancel_by_user_13(user)
+    mail to: user.email, body: I18n.t('.body', scope: scope, dashboard_link: dashboard_link, client: client(user))
+  end
+
+  def cancel_by_user_due_conf_delay_14(user)
+    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user))
+  end
+
+  def translator_approving_15(user)
+    I18n.locale = 'zh-CN'
+    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
+                                      dashboard_link: dashboard_link)
+  end
+
+  def new_order_for_translator_16(user)
+    I18n.locale = 'zh-CN'
+    mail to: user.email, body: I18n.t('.body', scope: scope, dashboard_link: dashboard_link, client: client(user))
+  end
+
+  # old: become_main_int
+  def become_main_int_17(user)
+    I18n.locale = 'zh-CN'
+    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
+                                      dashboard_link: dashboard_link)
+  end
+
+  #old: become_back_up_int
+  def become_back_up_int_18(user)
+    I18n.locale = 'zh-CN'
+    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
+                                      dashboard_link: dashboard_link)
+  end
+
+  #old: re_confirm_main
+  def re_confirm_main_19(user)
+    I18n.locale = 'zh-CN'
+    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
+                                      dashboard_link: dashboard_link)
+  end
+
+  def re_confirm_back_up_20(user)
+    I18n.locale = 'zh-CN'
+    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
+                                      dashboard_link: dashboard_link)
+  end
+
   def re_confirmed_translator_21(user)
+    I18n.locale = 'zh-CN'
     mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user),
                                       dashboard_link: (dashboard_link))
   end
-
-  def re_confirmed_client_6(user, offer)
-    mail to: user.email, body: I18n.t('.body', scope: scope, client: client(user), client_id: offer.order.number,
-                                      order_details: order_details(offer.order),
-                                      interpreter_link: "#{Rails.application.config.host}/#{asset_host}/get_pdf_translator/#{offer.translator.id.to_s}.pdf",
-                                      dashboard_link: dashboard_link)
-
-  end
-
 
   private
 
