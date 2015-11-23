8a7d1d6f790bcf2bcef10a53777dcaf402ea5ff9
mitchenkod <mitchenko1897@mail.ru>
2015-11-23 15:49:11 +0600

    #y-808 fixed work 10m

---
+++ spec/jobs/order_workflow_worker_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe OrderWorkflowWorker, type: :worker do
+
+  describe '#perform' do
+
+    let(:order) {create :order_verbal}
+
+    subject{OrderWorkflowWorker.new.perform order, 'after_12'}
+
+    it do
+      expect_any_instance_of(Order::Verbal::EventsService).to receive 'after_12'
+      subject
+    end
+
+  end
+
+end
