be81869daa605ba4be5bcaabac6bb69406908168
leo <mvleo.github@gmail.com>
2015-12-10 18:24:32 +0600

    #y-843 fixed

--- app/services/order/reject_service.rb
+++ app/services/order/reject_service.rb
@@ -2,6 +2,7 @@ module Order
   class RejectService
     def initialize(order)
       @order = order
+      @state = order.state
     end
 
     def reject_order(inner = :client)
@@ -12,18 +13,15 @@ module Order
     end
 
     def refund(inner = :client)
-      unless @order.invoices.first.pay_way.gateway_type == 'bank'
-        if @order.paid? && @order.owner.present?
-          sum = calculate_sum inner
-          # sum = 0 if @order.state.new && @order.invoices.first.pay_way.gateway_type == 'bank'
-          if sum > 0
-            tr = Transaction.create debit: Office.head,
-                                    credit: @order.owner.user,
-                                    sum: sum,
-                                    message: 'Refund',
-                                    subject: @order
-            tr.execute
-          end
+      if @state != 'new' && @state != 'paying' && @order.owner.present?
+        sum = calculate_sum inner
+        if sum > 0
+          tr = Transaction.create debit: Office.head,
+                                  credit: @order.owner.user,
+                                  sum: sum,
+                                  message: 'Refund',
+                                  subject: @order
+          tr.execute
         end
       end
     end
